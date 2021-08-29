5234b7b8cde54d172d4eaf65d47f33e9ad29247a
Brett Hughes <bretthughes12@optusnet.com.au>
2021-08-29 13:14:15 +1000

    Cosmetic fiddling with simpleform config

--- config/initializers/simple_form_bootstrap.rb
+++ config/initializers/simple_form_bootstrap.rb
@@ -408,26 +408,26 @@ SimpleForm.setup do |config|
 
   # Custom wrappers for input types. This should be a hash containing an input
   # type as key and the wrapper that will be used for all inputs with specified type.
-  config.wrapper_mappings = {
-    boolean:       :vertical_boolean,
-    check_boxes:   :vertical_collection,
-    date:          :vertical_multi_select,
-    datetime:      :vertical_multi_select,
-    file:          :vertical_file,
-    radio_buttons: :vertical_collection,
-    range:         :vertical_range,
-    time:          :vertical_multi_select
-  }
+  # config.wrapper_mappings = {
+  #   boolean:       :vertical_boolean,
+  #   check_boxes:   :vertical_collection,
+  #   date:          :vertical_multi_select,
+  #   datetime:      :vertical_multi_select,
+  #   file:          :vertical_file,
+  #   radio_buttons: :vertical_collection,
+  #   range:         :vertical_range,
+  #   time:          :vertical_multi_select
+  #  }
 
   # enable custom form wrappers
-  # config.wrapper_mappings = {
-  #   boolean:       :custom_boolean,
-  #   check_boxes:   :custom_collection,
-  #   date:          :custom_multi_select,
-  #   datetime:      :custom_multi_select,
-  #   file:          :custom_file,
-  #   radio_buttons: :custom_collection,
-  #   range:         :custom_range,
-  #   time:          :custom_multi_select
-  # }
+  config.wrapper_mappings = {
+    boolean:       :custom_boolean,
+    check_boxes:   :custom_collection,
+    date:          :custom_multi_select,
+    datetime:      :custom_multi_select,
+    file:          :custom_file,
+    radio_buttons: :custom_collection,
+    range:         :custom_range,
+    time:          :custom_multi_select
+  }
 end
