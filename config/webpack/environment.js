21c6b46db7b6f1e90b6fadf9cc86c64a7055e5a2
Brett Hughes <bretthughes12@optusnet.com.au>
2021-08-30 20:33:53 +1000

    Generated controllers and views for devise users to allow configuration

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -5,7 +5,7 @@ const webpack = require("webpack")
 environment.plugins.append("Provide", new webpack.ProvidePlugin({
   $: 'jquery',
   jQuery: 'jquery',
-  Popper: ['popper.js', 'default']
+  Popper: ['@popper.js/core', 'default']
 }))
 
 module.exports = environment
