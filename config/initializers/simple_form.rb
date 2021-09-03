9bdd0e81f5a467c6c686d0fb96531c51613a0a9f
Brett Hughes <bretthughes12@optusnet.com.au>
2021-09-03 21:21:20 +1000

    Style tweaks

--- config/initializers/simple_form.rb
+++ config/initializers/simple_form.rb
@@ -74,12 +74,12 @@ SimpleForm.setup do |config|
   config.boolean_style = :nested
 
   # Default class for buttons
-  config.button_class = 'btn'
+  config.button_class = 'btn-primary'
 
   # Method used to tidy up errors. Specify any Rails Array method.
   # :first lists the first message for each field.
   # Use :to_sentence to list all errors for each field.
-  # config.error_method = :first
+  # config.error_method = :firstespond_to(:this_year)
 
   # Default tag used for error notification helper.
   config.error_notification_tag = :div
