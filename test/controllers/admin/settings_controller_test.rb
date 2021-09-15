d021345127a8705020e971a703ac5a3d69e4114c
Brett Hughes <bretthughes12@optusnet.com.au>
2021-09-16 08:29:21 +1000

    Fleshed out some test coverage

--- test/controllers/admin/settings_controller_test.rb
+++ test/controllers/admin/settings_controller_test.rb
@@ -81,6 +81,16 @@ class Admin::SettingsControllerTest < ActionDispatch::IntegrationTest
     assert_equal true, @setting.syg_is_happening
   end
 
+  test "should not update functionality settings with errors" do
+    patch update_functionality_admin_setting_url(@setting), params: { setting: { new_group_sports_allocation_factor: 10.5 } }
+
+    assert_response :success
+    # Reload association to fetch updated data and assert that title is updated.
+    @setting.reload
+
+    assert_not_equal 10.5, @setting.new_group_sports_allocation_factor
+  end
+
   test "should update fee settings" do
     patch update_fees_admin_setting_url(@setting), params: { setting: { full_fee: 120 } }
 
@@ -89,6 +99,16 @@ class Admin::SettingsControllerTest < ActionDispatch::IntegrationTest
     @setting.reload
 
     assert_equal 120, @setting.full_fee
+  end
+
+  test "should not update fees settings with errors" do
+    patch update_fees_admin_setting_url(@setting), params: { setting: { full_fee: "a" } }
+
+    assert_response :success
+    # Reload association to fetch updated data and assert that title is updated.
+    @setting.reload
+
+    assert_not_equal "a", @setting.full_fee
   end
 
   test "should update division settings" do
@@ -101,6 +121,16 @@ class Admin::SettingsControllerTest < ActionDispatch::IntegrationTest
     assert_equal 10, @setting.small_division_ceiling
   end
 
+  test "should not update division settings with errors" do
+    patch update_divisions_admin_setting_url(@setting), params: { setting: { small_division_ceiling: "a" } }
+
+    assert_response :success
+    # Reload association to fetch updated data and assert that title is updated.
+    @setting.reload
+
+    assert_not_equal "a", @setting.small_division_ceiling
+  end
+
   test "should update sports allocation factors" do
     patch update_sports_factors_admin_setting_url(@setting), params: { setting: { new_group_sports_allocation_factor: 10 } }
 
@@ -109,5 +139,15 @@ class Admin::SettingsControllerTest < ActionDispatch::IntegrationTest
     @setting.reload
 
     assert_equal 10, @setting.new_group_sports_allocation_factor
+  end
+
+  test "should not update sports allocation factors with errors" do
+    patch update_sports_factors_admin_setting_url(@setting), params: { setting: { new_group_sports_allocation_factor: 10.5 } }
+
+    assert_response :success
+    # Reload association to fetch updated data and assert that title is updated.
+    @setting.reload
+
+    assert_not_equal "10.5", @setting.new_group_sports_allocation_factor
   end
 end
