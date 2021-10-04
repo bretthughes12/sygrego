62123f62d952a11b910016181c8c371e360a2b6f
Brett Hughes <bretthughes12@optusnet.com.au>
2021-10-04 19:24:48 +1100

    Added tests for users controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,4 +23,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_and_belongs_to_many :roles
+
+  validates :email,     presence: true,
+                        uniqueness: { case_sensitive: false }
 end
--- test/controllers/admin/users_controller_test.rb
+++ test/controllers/admin/users_controller_test.rb
@@ -1,7 +1,98 @@
 require "test_helper"
 
 class Admin::UsersControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  include Devise::Test::IntegrationHelpers 
+
+  def setup
+    FactoryBot.create(:setting)
+    @user1 = FactoryBot.create(:user)
+    @user = FactoryBot.create(:user)
+    
+    sign_in @user
+  end
+
+  test "should get index" do
+    get admin_users_url
+
+    assert_response :success
+  end
+
+  test "should show user" do
+    get admin_user_url(@user1)
+
+    assert_response :success
+  end
+
+  test "should not show non existent user" do
+    assert_raise(ActiveRecord::RecordNotFound) {
+      get admin_user_url(12345678)
+    }
+  end
+
+  test "should get new" do
+    get new_admin_user_url
+
+    assert_response :success
+  end
+
+  test "should create user" do
+    assert_difference('User.count') do
+      post admin_users_path, params: { user: FactoryBot.attributes_for(:user) }
+    end
+
+    assert_response :success
+    assert_match /successfully created/, flash[:notice]
+  end
+
+  test "should not create user with errors" do
+    assert_no_difference('User.count') do
+      post admin_users_path, params: { 
+                                user: FactoryBot.attributes_for(:user,
+                                email: @user1.email) }
+    end
+
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get edit_admin_user_url(@user1)
+
+    assert_response :success
+  end
+
+  test "should update user" do
+    patch admin_user_url(@user1), params: { user: { email: "test@example.com" } }
+
+    assert_redirected_to admin_users_path
+    assert_match /successfully updated/, flash[:notice]
+
+    # Reload association to fetch updated data and assert that title is updated.
+    @user1.reload
+
+    assert_equal "test@example.com", @user1.email
+  end
+
+  test "should not update user with errors" do
+    patch admin_user_url(@user1), params: { user: { email: @user.email } }
+
+    assert_response :success
+    # Reload association to fetch updated data and assert that title is updated.
+    @user1.reload
+
+    assert_not_equal @user.email, @user1.email
+  end
+
+  test "should destroy user" do
+    assert_difference("User.count", -1) do
+      delete admin_user_url(@user1)
+    end
+
+    assert_redirected_to admin_users_path
+  end
+
+  test "should not destroy non existent user" do
+    assert_raise(ActiveRecord::RecordNotFound) {
+      delete admin_user_url(12345678)
+    }
+  end
 end
