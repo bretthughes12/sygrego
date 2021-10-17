518be48eddf1614ff23bac69c03c61c2a359d0af
Brett Hughes <bretthughes12@optusnet.com.au>
2021-10-17 12:14:48 +1100

    Fleshed out testing for event details

--- test/controllers/admin/event_details_controller_test.rb
+++ test/controllers/admin/event_details_controller_test.rb
@@ -1,7 +1,127 @@
 require "test_helper"
 
 class Admin::EventDetailsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  include Devise::Test::IntegrationHelpers 
+
+  def setup
+    FactoryBot.create(:setting)
+    FactoryBot.create(:role, name: 'admin')
+    @user = FactoryBot.create(:user)
+    @event_detail = FactoryBot.create(:event_detail)
+    
+    sign_in @user
+  end
+
+  test "should get index" do
+    get admin_event_details_url
+
+    assert_response :success
+  end
+
+  test "should download event detail data" do
+    get admin_event_details_url(format: :csv)
+
+    assert_response :success
+    assert_match %r{text\/csv}, @response.content_type
+  end
+
+  test "should show event_detail" do
+    get admin_event_detail_url(@event_detail)
+
+    assert_response :success
+  end
+
+  test "should not show non existent event_detail" do
+    assert_raise(ActiveRecord::RecordNotFound) {
+      get admin_event_detail_url(12345678)
+    }
+  end
+
+  test "should get edit" do
+    get edit_admin_event_detail_url(@event_detail)
+
+    assert_response :success
+  end
+
+  test "should update event detail" do
+    patch admin_event_detail_url(@event_detail), params: { event_detail: { estimated_numbers: 24 } }
+
+    assert_redirected_to admin_event_details_path
+    assert_match /successfully updated/, flash[:notice]
+
+    # Reload association to fetch updated data and assert that title is updated.
+    @event_detail.reload
+
+    assert_equal 24, @event_detail.estimated_numbers
+  end
+
+  test "should not update event detail with errors" do
+    patch admin_event_detail_url(@event_detail), params: { event_detail: { buddy_interest: "a" } }
+
+    assert_response :success
+    # Reload association to fetch updated data and assert that title is updated.
+    @event_detail.reload
+
+    assert_not_equal "a", @event_detail.buddy_interest
+  end
+
+  test "should get new import" do
+    get new_import_admin_event_details_url
+
+    assert_response :success
+  end
+
+  test "should import event details" do
+    group = FactoryBot.create(:group, abbr: "CAF")
+    file = fixture_file_upload('event_detail.csv','application/csv')
+
+    assert_difference('EventDetail.count') do
+      post import_admin_event_details_url, params: { event_detail: { file: file }}
+    end
+
+    assert_redirected_to admin_event_details_path 
+    assert_match /upload complete/, flash[:notice]
+  end
+
+  test "should not import event_details when the file is not csv" do
+    group = FactoryBot.create(:group, abbr: "CAF")
+    file = fixture_file_upload('not_csv.txt','application/text')
+
+    assert_no_difference('EventDetail.count') do
+      post import_admin_event_details_url, params: { event_detail: { file: file }}
+    end
+
+    assert_response :success
+    assert_match /must be in '\.csv' format/, flash[:notice]
+  end
+
+  test "should show event_detail via xhr" do
+    sign_out @user
+
+    get admin_event_detail_url(@event_detail, format: :xml),
+        xhr: true,
+        headers: {'HTTP_AUTHORIZATION' => ActionController::HttpAuthentication::Basic.encode_credentials(@user.email, @user.password)}
+
+    assert_response :success
+  end
+
+  test "should not show event_detail via xhr when not authorised" do
+    sign_out @user
+
+    get admin_event_detail_url(@event_detail, format: :xml),
+        xhr: true,
+        headers: {}
+
+    assert_response 401
+  end
+
+  test "should not show non existent event_detail via xhr" do
+    sign_out @user
+
+    get admin_event_detail_url(123456, format: :xml),
+        xhr: true,
+        headers: {'HTTP_AUTHORIZATION' => ActionController::HttpAuthentication::Basic.encode_credentials(@user.email, @user.password)}
+
+    assert_response 404
+  end
 end
--- test/models/event_detail_test.rb
+++ test/models/event_detail_test.rb
@@ -33,7 +33,72 @@
 require "test_helper"
 
 class EventDetailTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  include ActionDispatch::TestProcess
+
+  def setup
+    FactoryBot.create(:role, name: 'admin')
+    @user = FactoryBot.create(:user)
+    @setting = FactoryBot.create(:setting)
+    @event_detail = FactoryBot.create(:event_detail)
+  end
+
+  test "should import event details from file" do
+    group = FactoryBot.create(:group, abbr: 'CAF')
+    file = fixture_file_upload('event_detail.csv','application/csv')
+    
+    assert_difference('EventDetail.count') do
+      @result = EventDetail.import(file, @user)
+    end
+
+    assert_equal 1, @result[:creates]
+    assert_equal 0, @result[:updates]
+    assert_equal 0, @result[:errors]
+  end
+
+  test "should update exiting event details from file" do
+    group = FactoryBot.create(:group, abbr: 'CAF')
+    event_detail = FactoryBot.create(:event_detail, group: group)
+    file = fixture_file_upload('event_detail.csv','application/csv')
+    
+    assert_no_difference('EventDetail.count') do
+      @result = EventDetail.import(file, @user)
+    end
+
+    assert_equal 0, @result[:creates]
+    assert_equal 1, @result[:updates]
+    assert_equal 0, @result[:errors]
+
+    event_detail.reload
+    assert_equal 34, event_detail.estimated_numbers
+  end
+
+  test "should not import event details with errors from file" do
+    group = FactoryBot.create(:group, abbr: 'CAF')
+    file = fixture_file_upload('invalid_event_detail.csv','application/csv')
+    
+    assert_no_difference('EventDetail.count') do
+      @result = EventDetail.import(file, @user)
+    end
+
+    assert_equal 0, @result[:creates]
+    assert_equal 0, @result[:updates]
+    assert_equal 1, @result[:errors]
+  end
+
+  test "should not update event details with errors from file" do
+    group = FactoryBot.create(:group, abbr: "CAF")
+    event_detail = FactoryBot.create(:event_detail, group: group)
+    file = fixture_file_upload('invalid_event_detail.csv','application/csv')
+    
+    assert_no_difference('EventDetail.count') do
+      @result = EventDetail.import(file, @user)
+    end
+
+    assert_equal 0, @result[:creates]
+    assert_equal 0, @result[:updates]
+    assert_equal 1, @result[:errors]
+
+    event_detail.reload
+    assert_not_equal "Not for us", event_detail.buddy_interest
+  end
 end
