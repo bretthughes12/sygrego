6e8c9ff8e7062a17cbf62cc87342b6a1546f7270
Brett Hughes <bretthughes12@optusnet.com.au>
2021-10-21 21:16:38 +1100

    Fixed issue with new user signups

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,8 @@ Rails.application.routes.draw do
 
   devise_for :users, controllers: {
     sessions: 'users/sessions',
-    passwords: 'users/passwords'
+    passwords: 'users/passwords',
+    registrations: 'users/registrations'
   } 
 
   resources :pages, only: [:show]
