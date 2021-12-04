9924702e436e9de10f77b6bb9ccce3b778cf8ba7
Brett Hughes <bretthughes12@optusnet.com.au>
2021-12-05 09:07:09 +1100

    Seer charts and tweaks

--- app/controllers/admin/info_controller.rb
+++ app/controllers/admin/info_controller.rb
@@ -6,8 +6,17 @@ class Admin::InfoController < ApplicationController
     # GET /admin/info/home
     def home
         @total_groups = Group.coming.count
+        new_groups = Group.coming.not_admin.new_group.count
+        last_year_groups = Group.coming.not_admin.last_year.count
+        admin_groups = Group.coming.is_admin.count
         @total_participants = Participant.accepted.coming.count
 
+        @groups_data = []
+        @groups_data << ChartData.new("New groups", new_groups)
+        @groups_data << ChartData.new("Came " + (@settings.this_year - 1).to_s, last_year_groups)
+        @groups_data << ChartData.new("Came pre-" + (@settings.this_year - 1).to_s, @total_groups - new_groups - last_year_groups - admin_groups)
+        @groups_data << ChartData.new("Admin", admin_groups)
+    
         @participants_data = []
         @participants_data << ChartData.new("Spectators", Participant.coming.accepted.spectators.count)
         @participants_data << ChartData.new("Playing", Participant.coming.accepted.playing_sport.count)
--- app/controllers/gc/info_controller.rb
+++ app/controllers/gc/info_controller.rb
@@ -7,5 +7,12 @@ class Gc::InfoController < ApplicationController
     # GET /gc/info/home
     def home
         @total_groups = Group.coming.count
+        @total_participants = Participant.coming.count
+        @participants_registered = @group.participants.coming.accepted.count
+
+        @participants_data = []
+        @participants_data << ChartData.new("Spectators", @group.participants.coming.accepted.spectators.count)
+        @participants_data << ChartData.new("Playing", @group.participants.coming.accepted.playing_sport.count)
+        @participants_data << ChartData.new("Not Approved", @group.participants.requiring_approval.count)
     end
 end
