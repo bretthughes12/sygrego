79d1a749c86f0aad95b2f2715ee7a534e4f9bae4
Brett Hughes <bretthughes12@optusnet.com.au>
2021-12-30 12:25:07 +1100

    Group Signup workflow tweaks

--- app/controllers/group_signups_controller.rb
+++ app/controllers/group_signups_controller.rb
@@ -5,7 +5,7 @@ class GroupSignupsController < ApplicationController
     # GET /group_signups/new
     def new
       @group_signup = GroupSignup.new
-      @groups = Group.order(:name).load
+      @groups = Group.stale.order(:name).load
   
       respond_to do |format|
         format.html # new.html.erb
@@ -23,20 +23,15 @@ class GroupSignupsController < ApplicationController
           @gc = @group_signup.gc
           @church_rep = @group_signup.church_rep
           
-#          if @group.lock_version == 0
-#            log(@group, "CREATE")
-#          else
-#            log(@group, "UPDATE")
-#          end
-    
           GroupMailer.new_group_signup(@group, @church_rep, @gc).deliver_now
           UserMailer.welcome_church_rep(@church_rep).deliver_now
           @token = @gc.get_reset_password_token
           UserMailer.gc_nomination(@gc, @group, @church_rep, @token).deliver_now
   
-#          @group.mysyg_enabled = @settings.mysyg_default_enabled
-#          @group.mysyg_open = @settings.mysyg_default_open
-#          @group.save
+          @group.reload
+          @group.mysyg_setting.mysyg_enabled = @settings.mysyg_default_enabled
+          @group.mysyg_setting.mysyg_open = @settings.mysyg_default_open
+          @group.save
   
           flash[:notice] = "Thank you for registering your group. You are signed in as #{@church_rep.name}. Please set your password."
           bypass_sign_in @church_rep
