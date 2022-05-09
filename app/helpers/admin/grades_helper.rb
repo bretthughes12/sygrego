5f7e6c23e58314f43faa1c9b0e06460c789deb8c
Brett Hughes <bretthughes12@optusnet.com.au>
2022-05-09 13:45:23 +1000

    Cosmetic fix

--- app/helpers/admin/grades_helper.rb
+++ app/helpers/admin/grades_helper.rb
@@ -1,10 +1,10 @@
 module Admin::GradesHelper
     def grade_display_class(grade)
       case 
-      when grade.one_entry_per_group?
-        'table-warning'
       when grade.over_limit?
         'table-danger'
+      when grade.one_entry_per_group?
+        'table-warning'
       else
         'table-primary'
       end
