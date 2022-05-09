07793d844fbe30fc64aff4ca19d05342f5e36e76
Brett Hughes <bretthughes12@optusnet.com.au>
2022-05-09 11:27:52 +1000

    Added row classes for grades

--- app/helpers/admin/grades_helper.rb
+++ app/helpers/admin/grades_helper.rb
@@ -1,6 +1,13 @@
 module Admin::GradesHelper
     def grade_display_class(grade)
-
+      case 
+      when grade.one_entry_per_group?
+        'table-warning'
+      when grade.over_limit?
+        'table-danger'
+      else
+        'table-primary'
+      end
     end
 
     def grade_filtered(grade, group)
