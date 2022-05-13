f77dff12b1d2457bb35cff3e23ee943c42a67484
Brett Hughes <bretthughes12@optusnet.com.au>
2022-05-13 14:51:40 +1000

    Bug fix from when dependent destroy is deleting participants_sport_entries records

--- app/models/participant.rb
+++ app/models/participant.rb
@@ -79,7 +79,7 @@ class Participant < ApplicationRecord
     belongs_to :group
     belongs_to :voucher, optional: true
     has_many   :volunteers
-    has_many   :participants_sport_entries, dependent: :destroy
+    has_many   :participants_sport_entries #, dependent: :destroy
 #    has_many   :fee_audit_trails
     has_many   :sport_entries, through: :participants_sport_entries
     has_many   :sport_preferences, dependent: :destroy
