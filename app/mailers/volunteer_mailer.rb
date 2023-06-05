9cd316bbcde2dfacfe1f6a83cc1e2640773a656d
Brett Hughes <bretthughes12@optusnet.com.au>
2023-06-05 19:19:43 +1000

    SC emails ready for 2023

--- app/mailers/volunteer_mailer.rb
+++ app/mailers/volunteer_mailer.rb
@@ -1,7 +1,6 @@
 class VolunteerMailer < ActionMailer::Base
     default from: 'sportadmin@stateyouthgames.com',
-            cc:   'registrations@stateyouthgames.com'
-#            cc:   ['sports@stateyouthgames.com', 'sportadmin@stateyouthgames.com', 'registrations@stateyouthgames.com']
+            cc:   ['sports@stateyouthgames.com', 'sportadmin@stateyouthgames.com', 'registrations@stateyouthgames.com']
     layout 'mailer'
     before_action :get_settings
     
@@ -9,27 +8,6 @@ class VolunteerMailer < ActionMailer::Base
       @sport = volunteer.sport
       @section = volunteer.section
       
-      unless volunteer.sport.nil?
-#        map_type = DownloadType.find_by_name("Maps - Sports Venues")
-#        @maps = map_type.downloads if map_type
-        @maps = nil
-#        info_type = DownloadType.find_by_name("Sports Information & Rules Booklet")
-#        @info = info_type.downloads if info_type
-        @info = nil
-#        @rules = @sport.downloads
-        
-#        case
-#          when @sport.draw_type == "Round Robin"
-#            how_to_type = DownloadType.find_by_name("Sports Officials - How to Complete the Ladder Worksheet")
-#            @how_tos = how_to_type.downloads if how_to_type
-        
-#          when @sport.draw_type == "Knockout"
-#            how_to_type = DownloadType.find_by_name("Sports Officials - How to Complete a Knockout")
-#            @how_tos = how_to_type.downloads if how_to_type
-#        end
-        
-      end
-  
       mail(to:      volunteer.email_recipients, 
            subject: "#{APP_CONFIG[:email_subject]} SYG #{APP_CONFIG[:this_year]} - Welcome Sports Coordinators")
     end
