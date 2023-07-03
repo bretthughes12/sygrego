0d6a24e3f8892c3570d5bc3b5eee02ac7a41023f
Brett Hughes <bretthughes12@optusnet.com.au>
2023-07-03 19:59:14 +1000

    Allow sports that can include negative scores to include them in results

--- app/helpers/admin/sports_helper.rb
+++ app/helpers/admin/sports_helper.rb
@@ -1,2 +1,9 @@
 module Admin::SportsHelper
+    def score_range(sport)
+        if sport.allow_negative_score
+            [*-99..-1, 'Forfeit', *0..99]
+        else
+            ['Forfeit', *0..99]
+        end
+    end
 end
