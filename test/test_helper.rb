ac3a282e0f8488727c7a5aad9a3183454ab6a37c
Brett Hughes <bretthughes12@optusnet.com.au>
2023-07-07 22:19:09 +1000

    Test  helper update to resolvee simplecov statistics

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -8,7 +8,7 @@ require 'mocha/minitest'
 
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
-  parallelize(workers: :number_of_processors)
+  # parallelize(workers: :number_of_processors)
 
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   # fixtures :all
