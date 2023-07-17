40f45e2c659bbd124f986498909c56d7374083dc
Brett Hughes <bretthughes12@optusnet.com.au>
2023-07-17 22:28:30 +1000

    Added test coverage for round robin matches helper

---
+++ test/helpers/admin/round_robin_matches_helper_test.rb
@@ -0,0 +1,18 @@
+require File.dirname(__FILE__) + '/../../test_helper'
+require 'admin/groups_helper'
+
+class Admin::RoundRobinMatchesHelperTest < ActionView::TestCase
+  include Admin::RoundRobinMatchesHelper
+  
+  def setup
+    FactoryBot.create(:role, name: 'admin')
+    @user = FactoryBot.create(:user)
+    @setting = FactoryBot.create(:setting)
+    @match = FactoryBot.create(:round_robin_match)
+  end
+  
+  test "forfeit display class" do
+    assert_equal "table-danger", forfeit_display_class(true)
+    assert_equal "table-dark", forfeit_display_class(false)
+  end
+end  
