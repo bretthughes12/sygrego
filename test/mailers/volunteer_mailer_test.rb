aebd5d9b57016a030bc35581a85d64794d21b0a2
Brett Hughes <bretthughes12@optusnet.com.au>
2023-07-20 20:45:51 +1000

    Added test coverage for volunteer mailer

---
+++ test/mailers/volunteer_mailer_test.rb
@@ -0,0 +1,26 @@
+require "test_helper"
+
+class VolunteerMailerTest < ActionMailer::TestCase
+    tests VolunteerMailer
+  
+    def setup
+      @admin_role = FactoryBot.create(:role, name: 'admin')
+      @user = FactoryBot.create(:user)
+      @setting = FactoryBot.create(:setting)
+      @sport_coord = FactoryBot.create(:volunteer)
+    end
+    
+    def test_sport_coordinator_email
+      group = FactoryBot.create(:group)
+      FactoryBot.create(:event_detail, group: group)
+      section = FactoryBot.create(:section)
+      participant = FactoryBot.create(:participant, group: group)
+      volunteer = FactoryBot.create(:volunteer, section: section, participant: participant)
+      
+      email = VolunteerMailer.welcome(volunteer)
+      
+      assert_match /Welcome Sports Coordinators/, email.subject  
+      assert_equal volunteer.email_recipients, email.to[0]
+      assert_match /Hi SYG sports co-ordinator/, email.parts[0].body.to_s
+    end
+  end
