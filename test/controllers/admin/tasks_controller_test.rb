517990dc3da36a59f2df5082307d542d33a3290d
Brett Hughes <bretthughes12@optusnet.com.au>
2023-08-20 12:59:27 +1000

    Some improvements to test coverage of Admin controllers

--- test/controllers/admin/tasks_controller_test.rb
+++ test/controllers/admin/tasks_controller_test.rb
@@ -4,7 +4,7 @@ class Admin::TasksControllerTest < ActionDispatch::IntegrationTest
   include Devise::Test::IntegrationHelpers 
 
   def setup
-    FactoryBot.create(:setting)
+    @setting = FactoryBot.create(:setting)
     FactoryBot.create(:audit_trail)
     @user = FactoryBot.create(:user, :admin)
     
@@ -15,5 +15,65 @@ class Admin::TasksControllerTest < ActionDispatch::IntegrationTest
     get sports_draws_admin_tasks_url
 
     assert_response :success
+  end
+
+  test "should allocate restricted sports" do
+    post allocate_restricted_admin_tasks_url
+
+    assert_redirected_to summary_admin_ballot_results_path
+    assert_match /allocation is under way/, flash[:notice]
+
+    @setting.reload
+    assert_equal true, @setting.restricted_sports_allocated
+  end
+
+  test "should not allocate restricted sports when already done" do
+    @setting.restricted_sports_allocated = true
+    @setting.save
+
+    post allocate_restricted_admin_tasks_url
+
+    assert_redirected_to summary_admin_ballot_results_path
+    assert_match /already allocated/, flash[:notice]
+  end
+
+  test "should finalise team sports" do
+    post finalise_team_sports_admin_tasks_url
+
+    assert_redirected_to sports_draws_admin_tasks_path
+    assert_match /team grades is under way/, flash[:notice]
+
+    @setting.reload
+    assert_equal true, @setting.team_draws_complete
+  end
+
+  test "should not finalise team sports when already done" do
+    @setting.team_draws_complete = true
+    @setting.save
+
+    post finalise_team_sports_admin_tasks_url
+
+    assert_redirected_to sports_draws_admin_tasks_path
+    assert_match /already finalised/, flash[:notice]
+  end
+
+  test "should finalise individual sports" do
+    post finalise_individual_sports_admin_tasks_url
+
+    assert_redirected_to sports_draws_admin_tasks_path
+    assert_match /individual grades is under way/, flash[:notice]
+
+    @setting.reload
+    assert_equal true, @setting.indiv_draws_complete
+  end
+
+  test "should not finalise individual sports when already done" do
+    @setting.indiv_draws_complete = true
+    @setting.save
+
+    post finalise_individual_sports_admin_tasks_url
+
+    assert_redirected_to sports_draws_admin_tasks_path
+    assert_match /already finalised/, flash[:notice]
   end
 end
