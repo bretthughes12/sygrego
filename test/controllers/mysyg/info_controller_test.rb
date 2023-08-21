a276007b8f5ceb7a438a1504d1556da1bbffa7ac
Brett Hughes <bretthughes12@optusnet.com.au>
2023-08-21 21:46:50 +1000

    Some improvements to test coverage of MySYG controllers

--- test/controllers/mysyg/info_controller_test.rb
+++ test/controllers/mysyg/info_controller_test.rb
@@ -27,4 +27,24 @@ class Mysyg::InfoControllerTest < ActionDispatch::IntegrationTest
 
     assert_response :success
   end
+
+  test "should get other home page when not participant" do
+    sign_out @user
+    gc_user = FactoryBot.create(:user, :gc)
+    group = FactoryBot.create(:group)
+    gc_user.groups << group
+    sign_in gc_user
+
+    get home_mysyg_info_url(group: @group.mysyg_setting.mysyg_name)
+
+    assert_redirected_to home_gc_info_url
+  end
+
+  test "should get sign in page when gc not signed in" do
+    sign_out @user
+
+    get home_mysyg_info_url(group: @group.mysyg_setting.mysyg_name)
+
+    assert_redirected_to new_user_session_url
+  end
 end
