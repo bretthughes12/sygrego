f656cd2f21aca8123a7cd4feeef179f72869ab2f
Brett Hughes <bretthughes12@optusnet.com.au>
2023-08-31 18:59:49 +1000

    Some improvements to test coverage of Jobs

--- test/jobs/model_audit_job_test.rb
+++ test/jobs/model_audit_job_test.rb
@@ -1,7 +1,22 @@
 require "test_helper"
 
 class ModelAuditJobTest < ActiveJob::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    FactoryBot.create(:role, name: 'admin')
+    @user = FactoryBot.create(:user)
+    @setting = FactoryBot.create(:setting)
+  end
+
+  test "audit record is created" do
+    sport = FactoryBot.create(:sport)
+
+    assert_difference('AuditTrail.count') do
+      ModelAuditJob.perform_now(sport, "CREATE")
+    end
+
+    audit = AuditTrail.last
+    assert_equal 'Sport', audit.record_type
+    assert_equal 'CREATE', audit.event
+    assert_nil audit.user_id
+  end
 end
