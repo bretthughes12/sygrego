86311ad02a1f410e5c13baf998b37f642941c86d
Brett Hughes <bretthughes12@optusnet.com.au>
2024-03-12 14:22:42 +1100

    Remove debugging and remove bold styling on  downloads

--- app/controllers/admin/venues_controller.rb
+++ app/controllers/admin/venues_controller.rb
@@ -91,10 +91,6 @@ class Admin::VenuesController < AdminController
 
         flash[:notice] = "Venues upload complete: #{result[:creates]} venues created; #{result[:updates]} updates; #{result[:errors]} errors"
 
-        if result[:errors].to_i > 0 
-          pp result[:error_list]
-        end
-
         respond_to do |format|
           format.html { redirect_to admin_venues_url }
         end
