a1019668fc183f0c3c599a1e21d0af349cb7e9f4
Brett Hughes <bretthughes12@optusnet.com.au>
2025-01-27 13:28:56 +1100

    Rails 8.0 update results

--- config/application.rb
+++ config/application.rb
@@ -17,20 +17,19 @@ Bundler.require(*Rails.groups)
 module Sygrego
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.1
-    
+    config.load_defaults 8.0
+
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
-    config.autoload_lib(ignore: %w(assets tasks))
-    
+    config.autoload_lib(ignore: %w[assets tasks])
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
     config.time_zone = "Melbourne"
-    config.active_support.to_time_preserves_timezone = :zone
     # config.eager_load_paths << Rails.root.join("extras")
 
     # Devise config
