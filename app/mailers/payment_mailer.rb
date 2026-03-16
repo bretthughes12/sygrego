1b5f0841d46989a4b28d79d32f1e84a5512da376
Brett Hughes <bretthughes12@optusnet.com.au>
2026-03-16 19:30:18 +1100

    Remove Juzzy from cc on receipts

--- app/mailers/payment_mailer.rb
+++ app/mailers/payment_mailer.rb
@@ -3,7 +3,6 @@ class PaymentMailer < ApplicationMailer
         @payment = payment
 
         mail(to:      @payment.group.email_recipients,
-             bcc:     @settings.admin_email,
              subject: "#{APP_CONFIG[:email_subject]} SYG Payment Receipt")
     end
 
