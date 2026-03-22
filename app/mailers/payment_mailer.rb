6eda4b7adfcc2ebd95f1a3b0b5482a930e93c3a3
Brett Hughes <bretthughes12@optusnet.com.au>
2026-03-22 13:33:35 +1100

    A couple minor updates

--- app/mailers/payment_mailer.rb
+++ app/mailers/payment_mailer.rb
@@ -2,16 +2,19 @@ class PaymentMailer < ApplicationMailer
     def receipt(payment)
         @payment = payment
 
-        mail(to:      @payment.group.email_recipients,
-             bcc:     @settings.admin_email,
-             subject: "#{APP_CONFIG[:email_subject]} SYG Payment Receipt")
+        unless @payment.group.email_recipients.nil?
+            mail(to:      @payment.group.email_recipients,
+                subject: "#{APP_CONFIG[:email_subject]} SYG Payment Receipt")
+        end
     end
 
     def invoice(invoice)
         @invoice = invoice
 
-        mail(to:      @invoice.group.email_recipients,
-             subject: "#{APP_CONFIG[:email_subject]} New SYG Invoice")
+        unless @invoice.group.email_recipients.nil?
+            mail(to:      @invoice.group.email_recipients,
+                subject: "#{APP_CONFIG[:email_subject]} New SYG Invoice")
+        end
     end
 end
   
