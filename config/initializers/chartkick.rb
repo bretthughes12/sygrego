bcedc6647d2190b19ac4e07e8a00aa48beff8404
Brett Hughes <bretthughes12@optusnet.com.au>
2026-04-30 20:58:55 +1000

    Reorder line charts so that most recent years are more prominent

--- config/initializers/chartkick.rb
+++ config/initializers/chartkick.rb
@@ -1,6 +1,6 @@
 Chartkick.options = {
     legend: "right",
-    colors: ['#07915D', '#10518B', '#54BE09', '#D74A0A'],
+    colors: ['#D74A0A', '#54BE09', '#10518B', '#07915D'],
     empty: "No data",
     loading: "Loading..."
 }
