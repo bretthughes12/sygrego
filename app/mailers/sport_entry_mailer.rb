fb3296e006e6a6b0564da3d27f0739e7251936c2
Brett Hughes <bretthughes12@optusnet.com.au>
2026-05-19 07:14:14 +1000

    Restore email settings after temporary provider problem

--- app/mailers/sport_entry_mailer.rb
+++ app/mailers/sport_entry_mailer.rb
@@ -6,22 +6,22 @@ class SportEntryMailer < ApplicationMailer
     def restricted_sport_withdrawal(entry)
       @entry = entry
   
-      # mail(subject: "#{APP_CONFIG[:email_subject]} Withdrawal from #{entry.section_name}")
+      mail(subject: "#{APP_CONFIG[:email_subject]} Withdrawal from #{entry.section_name}")
     end
   
     def restricted_sport_offer(entry)
       @entry = entry
-      entries_missed_out = @entry.grade.entries_waiting
+      # entries_missed_out = @entry.grade.entries_waiting
       notifies = @entry.grade.entries_waiting.collect do |e|
         e.group.email_recipients
       end.flatten.uniq
   
-      # mail(from:    @settings.sports_email,
-      #      bcc:     notifies,
-      #      subject: "#{APP_CONFIG[:email_subject]} Restricted Sports Offer - #{entry.section_name}") do |format|
-      #   format.html { render layout: 'mailer' }
-      #   format.text
-      # end
+      mail(from:    @settings.sports_email,
+           bcc:     notifies,
+           subject: "#{APP_CONFIG[:email_subject]} Restricted Sports Offer - #{entry.section_name}") do |format|
+        format.html { render layout: 'mailer' }
+        format.text
+      end
     end
   
     def draw_entry_withdrawal(entry)
